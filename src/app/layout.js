47b7e5626ae65d0fcd9aa42b0ebfdad3c8c62a70
김지섭 <cocacola158500@gmail.com>
2024-02-12 19:47:41 +0900

    connect prisma with mysql db

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import MenuBar from "@/components/MenuBar";
 import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <>
       <Header />
+      <MenuBar />
       {children}
       <Footer />
     </>
